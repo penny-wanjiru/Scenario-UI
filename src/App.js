cbb60257d92dbb98ead474aec3f7b28bb47c6d50
Penina Wanjiru <penina.wanjiru@andela.com>
2021-05-18 20:32:19 +0300

    Add fetch request to sign up form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
+import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link, Redirect } from "react-router-dom";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
+import {initialState, reducer} from './reducers/AuthReducer'
 import { Header } from './components/Header';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
 import Reset from './pages/PasswordReset';
 import Home from './pages/Dashboard';
 
+export const AuthContext = React.createContext();
+
 
 const App = () => {
+	const [state, dispatch] = React.useReducer(reducer, initialState);
   return (
-		<>
-		<Header/>
-		<Switch>
-			<Route exact path='/login' component={Login} />
-			<Route exact path='/signup' component={Signup} />
-			<Route exact path='/reset' component={Reset} />
-			<Route exact path='/dashboard' component={Home} />
-		</Switch>
-		</>
+		<AuthContext.Provider value={{state, dispatch}}>
+			<Header/>
+			<Switch>
+				<Route exact path='/' component={Login} />
+				<Route exact path='/signup' component={Signup} />
+				<Route exact path='/reset' component={Reset} />
+				<Route exact path='/dashboard' component={Home} />
+			</Switch>
+		</AuthContext.Provider>
   );
 }
 
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -4,25 +4,54 @@ import {Form, Button} from 'react-bootstrap';
 
 import { Formik, Field } from "formik";
 import {ValidationSchema} from '../schema/validation'
+import { AuthContext } from "../App";
 
 import { Wrapper, FormWrapper, FormCard, Message, Heading , Group, SubmitButton, Paragraph} from "../styles/forms";
 
 const Signup = () => {
+	const { dispatch } = React.useContext(AuthContext);
+
   return (
 		<Wrapper>
 			<FormWrapper>
 			<Formik
 				initialValues={{
-					email: 'init@gmail.com',
+					email: '',
 					password:'',
+					errorMessage: null
 				}}
 				validationSchema={ValidationSchema}
-				onSubmit={(values, {setSubmitting, resetForm}) => {
-					console.log(values)
+				onSubmit={(values, {setSubmitting, setErrors, resetForm}) => {
+					fetch("https://hiring-example-25770.botics.co/rest-auth/registration/", {
+						method: "post",
+						headers: {
+							"Content-Type": "application/json"
+						},
+						body: JSON.stringify({
+							email: values.email,
+							password: values.password
+						})
+					})
+						.then(res => {
+							if (res.ok) {
+								console.log("this is the res", res);
+								return res.json();
+							}
+							throw res;
+						})
+						.then(resJson => {
+							dispatch({
+								type: "REGISTRATION",
+								payload: resJson
+							});
+						})
+						.catch(error => {
+							setErrors({errorMessage: error.message || error.statusText})
+						});
 				}}
 				>
 				{( {values, errors, handleChange, handleBlur, handleSubmit, isSubmitting, isValid}) => (
-					<FormCard>
+					<FormCard onSubmit={handleSubmit}>
 						<Heading>Sign up</Heading>
 						<Group controlId="formBasicEmail">
 							<Form.Label>Email address</Form.Label>
@@ -38,11 +67,14 @@ const Signup = () => {
 								{errors.password}
 							</span>
 						</Group>
+						{errors.errorMessage && (
+              <span className="form-error">{errors.errorMessage}</span>
+            )}
 						<SubmitButton variant="primary" type="submit">
-							Sign Up
+							{isSubmitting ? "Signing up..." : "SignUp"}
 						</SubmitButton>
 						<Paragraph>
-							Already registered <a href="/login"> sign in? </a>
+							Already registered <a href="/"> sign in? </a>
 						</Paragraph>
 					</FormCard>
 				)}
