9958504ba98c8327e9d37264784b44e027965374
Penina Wanjiru <penina.wanjiru@andela.com>
2021-05-18 14:56:18 +0300

    Define initial components and pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import { Header } from './components/Header';
+import { Signup } from './pages/Signup';
+import { Login} from './pages/Login';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
---
+++ src/pages/Signup.js
@@ -0,0 +1,9 @@
+const Signup = () => {
+  return (
+    <div className="Signup">
+
+    </div>
+  );
+}
+
+export default Signup;
